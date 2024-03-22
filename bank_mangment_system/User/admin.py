72f830e1d77a00202b1a964d1dc225591c45e724
akanshabaishwade <akanshabaishwade@gmail.com>
2024-03-23 01:35:14 +0530

    [Fix] Customuser bug solve

--- bank_mangment_system/User/admin.py
+++ bank_mangment_system/User/admin.py
@@ -1,15 +1,15 @@
-from django.contrib import admin
-from .models import *
+# from django.contrib import admin
+# from .models import *
 
 
-# @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone_number', 'user_type')
-    fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'user_type', 'is_active')  
+# # @admin.register(CustomUser)
+# class CustomUserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'email', 'phone_number', 'user_type')
+#     fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 'user_type', 'is_active')  
 
 
 
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
+# admin.site.register(CustomUser, CustomUserAdmin)
 
