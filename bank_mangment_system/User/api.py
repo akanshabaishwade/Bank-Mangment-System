1913be387f468804810d9fbe27d5d5f0d0a0c269
akanshabaishwade <akanshabaishwade@gmail.com>
2024-03-23 01:49:26 +0530

    [add] login login Registration

---
+++ bank_mangment_system/User/api.py
@@ -0,0 +1,38 @@
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from .models import CustomUser
+from .serializers import CustomUserSerializer
+
+class CustomUserRegistration(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomUserLogin(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class CustomUserLogout(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            request.user.auth_token.delete()
+        except:
+            pass
+        return Response(status=status.HTTP_200_OK)
--- bank_mangment_system/User/urls.py
+++ bank_mangment_system/User/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .api import CustomUserRegistration, CustomUserLogin, CustomUserLogout
 
 urlpatterns = [
-
+    path('register/', CustomUserRegistration.as_view(), name='register'),
+    path('login/', CustomUserLogin.as_view(), name='login'),
+    path('logout/', CustomUserLogout.as_view(), name='logout'),
 ]
