3aba8a9aa0fe393c642a2c3c6fcab4b72510c78c
akanshabaishwade <akanshabaishwade@gmail.com>
2024-03-25 23:13:52 +0530

    [Fix] Bug Fix

--- bank_mangment_system/Customer/urls.py
+++ bank_mangment_system/Customer/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from . import api
 
 router = DefaultRouter()
@@ -10,4 +11,6 @@ router.register(r'deposit_slips', api.DepositSlipViewSet)
 router.register(r'withdrawal_slips', api.WithdrawalSlipViewSet)
 router.register(r'loan_payments', api.LoanPaymentViewSet)
 
-
+urlpatterns = [
+    path('', include(router.urls)),
+]
