6f44aa57368647f6e255e811c6bf6b8cdd694916
akanshabaishwade <akanshabaishwade@gmail.com>
2024-05-15 12:40:16 +0530

    [Add] vercel

--- bank_mangment_system/wsgi.py
+++ bank_mangment_system/wsgi.py
@@ -2,6 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bank_mangment_system.settings")
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bank_mangment_system.settings')
 application = get_wsgi_application()
